02a0567c86891d37aef840eb8fbb67b823c3dfc0
alcmanuel12 <alc.manuel12@gmail.com>
2025-09-30 14:37:29 +0200

    first commit

---
+++ Desktop/ASIGNATURAS/DW SERVIDOR/Actividades-formativas-sobre-Python-Django/Actividades.py
@@ -0,0 +1,53 @@
+
+#ACTIVIDAD 1
+
+#import os
+
+#decision = 1
+
+#if decision == 0:
+#    with open("mi_ruta.txt", "w") as f:
+#        f.write(os.getcwd())
+#
+#elif decision == 1:
+#    with open("mi_ruta.txt", "r") as f:
+#        datos = f.read()
+#        print("La ruta actual es " + datos)
+
+
+#Actividad 2
+#class Mascota:
+#
+#    def __init__(self,nombre, anioNac, nomDuenio = None):
+#        self.nombre = nombre
+#        self.anioNac = anioNac
+#        self.nomDuenio = nomDuenio
+#
+#    def calcular_Edad(self):
+#        anio_actual = 2025
+#        return anio_actual - self.anioNac
+#
+#    def adoptar(self, nombre_duenio):
+#        self.nomDuenio = nombre_duenio
+#        print(self.nomDuenio)
+#
+#    def __str__(self):
+#        return "Mi mascota " + self.nombre
+
+
+
+#ACTIVIDAD 3
+#num1 = 8
+#num2 = 8
+#num3 = 8
+
+#if num1 > num2 and num1 > num3:
+#    print("El numero mas alto es ", num1)
+#elif num2 > num1 and num2 > num3:
+#    print("El numero mas alto es ", num2)
+#elif num3 > num1 and num3 > num2:
+#    print("El numero mas alto es ", num3)
+#else:
+#    print("LOS NUMERO SON INGUALES")
+
+    
