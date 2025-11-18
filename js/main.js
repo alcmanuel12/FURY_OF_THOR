960c6b963f2c301a3e10de55314e79bb6c04ed62
VarozXYZ <alvaropiro@gmail.com>
2025-11-18 23:52:57 +0100

    updated background music

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ import { initIngameScreen } from './screens/ingameScreen.js';
 
 function registerSounds() {
     soundManager.registerSound('forest', 'https://res.cloudinary.com/din119ww9/video/upload/v1763026026/sonido-bosque_h3l0u8.mp3', true);
-    soundManager.registerSound('ingame', 'https://res.cloudinary.com/din119ww9/video/upload/v1763454899/ytmp3free.cc_hausbrjotr-skullcrusher-youtubemp3free.org_ljh9jh.mp3', true);
-    soundManager.registerSound('lightning-effect', 'https://res.cloudinary.com/diycpogap/video/upload/v1763504754/lightning-effect_mv9o91.mp3', false, 'effect');
+    soundManager.registerSound('ingame', 'https://res.cloudinary.com/diycpogap/video/upload/v1763506178/background-ingame-music_xr61ns.mp3', true), "background", 0.5;
+    soundManager.registerSound('lightning-effect', 'https://res.cloudinary.com/diycpogap/video/upload/v1763504754/lightning-effect_mv9o91.mp3', false, 'effect', 0.7);
 }
 
 function init() {
